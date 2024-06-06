0a440da26aa6291fa957b3ad843a6e0600292aff
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-06-06 17:00:47 +0200

    feat(infra): cache endpoint

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 from markdown_server.app import app as markdown_server_app
+from infrastructure.cache import cache
 
 app = Flask(__name__)
+cache.init_app(app)
 app.register_blueprint(markdown_server_app)
 
 
--- markdown_server/app.py
+++ markdown_server/app.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 
 from markdown_server import action
+from infrastructure.cache import cache
 
 app = Blueprint("markdown_server_app", __name__, template_folder="templates")
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
+@cache.cached(timeout=60)
 def index(path: str):
     return action.index(path)
